233f197bdce7a93d40cf0a542ca7da0d9d2c6712
jihaadbrb <jihadbourbab79@gmail.com>
2024-05-01 15:37:59 -0700

    jihad controllers

---
+++ app/Http/Controllers/FactureController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Facture;
+use Illuminate\Http\Request;
+
+class FactureController extends Controller
+{
+    public function index()
+    {
+        return view();
+    }
+    public function getFacture(){
+        $factures = Facture::All();
+        return view('',compact('views'));
+    }
+
+    public function createFacture(Request $request)
+    {
+        $validatedData = $request->validate([
+        'date_commande'=>'required|Date',
+        'nom_client'=>'required|String',
+        'prenom_client'=>'required|String',
+        'versement'=>'Number',
+        'reste'=>'Number',
+        'saisi_par'=>'String',
+        'saisi_le'=>'Date',
+        'total_TTC'=>'required|Number',
+
+        ]);
+
+        $newFacture = Facture::create($validatedData);
+        return redirect()->back();
+    }
+
+    public function updateFacture(Request $request,$id){
+
+        
+        $validatedData = $request->validate([
+            'date_commande'=>'required|Date',
+            'nom_client'=>'required|String',
+            'prenom_client'=>'required|String',
+            'versement'=>'numeric',
+            'reste'=>'numeric',
+            'saisi_par'=>'String',
+            'saisi_le'=>'Date',
+            'total_TTC'=>'required|numeric',
+    
+            ]);
+        
+            $facture = Facture::findOrFail($id);
+            $facture->update($validatedData);
+            return redirect()->back();
+        
+        
+    }
+
+    public function deleteFacture($id){
+        $facture = Facture::findOrFail($id);
+        $facture->delete();
+        return redirect()->back();
+    }
+}
---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index(){
+        return view();
+    }
+
+    public function getUsers(){
+        $users = User::All();
+        return view('',compact('users'));
+    }
+
+    public function createUser(Request $request){
+        $validatedData= $request->validate([
+            'username'=>'required|String',
+            'nom'=>'required|String',
+            'prenom'=>'required|String',
+            'password'=>'required|String',
+            'phonenumber'=>'required|String',
+        ]);
+        $newuser = User::create($validatedData);
+        return redirect()->back();
+    }
+
+    public function updateUser(Request $request,$id){
+        $validatedData= $request->validate([
+            'username'=>'required|String',
+            'nom'=>'required|String',
+            'prenom'=>'required|String',
+            'password'=>'required|String',
+            'phonenumber'=>'required|String',
+        ]);
+
+        $User = User::findOrFail($id);
+        $user->update($validatedData);
+        return redirect()->back();
+    }
+
+    public function deleteUser($id){
+        $user = User::findOrFail($id);
+        $user->delete();
+        return redirect()->back();
+    }
+}
