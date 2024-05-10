9dabcc76ef487c84143cf5563b24d4b4487d2456
ftouuh <zaydftouh79@gmail.com>
2024-05-10 23:36:56 +0100

    done

--- resources/views/admin/layouts/components/header.blade.php
+++ resources/views/admin/layouts/components/header.blade.php
@@ -2,39 +2,14 @@
     <div class="navbar-header">
         <div class="d-flex">
             <!-- LOGO -->
-            <div class="navbar-brand-box">
-                <a href="{{route('admin.dashboard')}}" class="logo logo-dark">
-                    <span class="logo-sm">
-                        {{-- <img src="assets/images/logo-sm.png" alt="logo-sm" height="22"> --}}
-                    </span>
-                    <span class="logo-lg">
-                        {{-- <img src="assets/images/logo-dark.png" alt="logo-dark" height="20"> --}}
-                    </span>
-                </a>
-
-                <a href="{{route('admin.dashboard')}}" class="logo logo-light">
-
-                    <span class="logo-sm">
-                        {{-- <img src="assets/images/logo-sm.png" alt="logo-sm-light" height="22"> --}}
-                    </span>
-                    <span class="logo-lg">
-                        {{-- <img src="assets/images/logo-light.png" alt="logo-light" height="20"> --}}
-                    </span>
-                </a>
-            </div>
+            
 
             <button type="button" class="btn btn-sm px-3 font-size-24 header-item waves-effect" id="vertical-menu-btn">
                 <i class="ri-menu-2-line align-middle"></i>
             </button>
 
             <!-- App Search-->
-            <form class="app-search d-none d-lg-block">
-                <div class="position-relative">
-                    <input type="text" class="form-control" placeholder="Search...">
-                    <span class="ri-search-line"></span>
-                </div>
-            </form>
-
+           
 
         </div>
 
